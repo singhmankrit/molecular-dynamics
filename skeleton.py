3acc864606527eeb630f5fcc47024ec2b479aadd
Noa Aarts <noa@voorwaarts.nl>
2025-02-12 10:49:58 +0100

    add some initial numpy arrays for the starting state

--- skeleton.py
+++ skeleton.py
@@ -6,6 +6,20 @@ you have a good reason to do so.
 
 import numpy as np
 
+init_pos = np.array(
+    [
+        [0.2, 0.5, 0.0],
+        [0.5, 0.8, 0.0],
+    ]
+)
+
+init_vel = np.array(
+    [
+        [0.1, 0.1, 0.0],
+        [0.6, 0.2, 0.0],
+    ]
+)
+
 
 def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
     """
@@ -23,7 +37,7 @@ def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
         The total number of simulation steps
     timestep : float
         Duration of a single simulation step
-    box_dim : float
+    box_dim : np.ndarray(float)
         Dimensions of the simulation box
 
     Returns
