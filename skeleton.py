958858eefd4d81882703eb41d5e91d7d11b9b691
Raghav Juyal <r.juyal@student.tudelft.nl>
2025-03-01 23:57:39 +0100

    Move simulation functions to a separate file

--- skeleton.py
+++ skeleton.py
@@ -6,199 +6,9 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 import initialisation
+import 
 from utilities import parse_config, dprint
 
-
-def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
-    """
-    Molecular dynamics simulation using the Euler or Verlet's algorithms
-    to integrate the equations of motion. Calculates energies and other
-    observables at each timestep.
-
-    Parameters
-    ----------
-    init_pos : np.ndarray
-        The initial positions of the atoms in Cartesian space
-    init_vel : np.ndarray
-        The initial velocities of the atoms in Cartesian space
-    num_tsteps : int
-        The total number of simulation steps
-    timestep : float
-        Duration of a single simulation step
-    box_dim : np.ndarray(float)
-        Dimensions of the simulation box
-
-    Returns
-    -------
-    Any quantities or observables that you wish to study.
-    """
-    positions = [init_pos]
-    velocities = [init_vel]
-
-    current_positions = init_pos
-    current_velocities = init_vel
-    dprint(
-        f"starting positions are {current_positions} and starting velocities are {current_velocities}"
-    )
-
-    # we calculate these so we can calculate the "next step" only from now on
-    relative_positions, distances = atomic_distances(current_positions, box_dim)
-    current_forces = lj_force(relative_positions, distances)
-
-    kinetic_energies = [kinetic_energy(init_vel)]
-    potential_energies = [potential_energy(distances)]
-    distance_list = [distances]
-
-    for step in np.arange(num_tsteps):
-        dprint(
-            f"""
-            at step {step} the particles are at {current_positions}
-            the particles have velocities {current_velocities}
-            """
-        )
-
-        # use velocity-verlet to calculate the new positions and velocities
-        current_positions = (
-            current_positions
-            + current_velocities * timestep
-            + current_forces * timestep * timestep / 2
-        ) % box_dim
-        relative_positions, distances = atomic_distances(current_positions, box_dim)
-        new_forces = lj_force(relative_positions, distances)
-        current_velocities += (current_forces + new_forces) * timestep / 2
-
-        # add the current statistics to the logs
-        kinetic_energies.append(kinetic_energy(current_velocities))
-        potential_energies.append(potential_energy(distances))
-        distance_list.append(distances)
-
-        # keep track of the positions and velocities
-        positions.append(current_positions)
-        velocities.append(current_velocities)
-
-        # update the forces so n -> n+1
-        current_forces = new_forces
-
-    return positions, velocities, kinetic_energies, potential_energies, distance_list
-
-
-def atomic_distances(
-    pos: np.typing.NDArray[np.float64], box_dim: np.typing.NDArray[np.float64]
-) -> np.typing.NDArray[np.float64]:
-    """
-    Calculates relative positions and distances between particles.
-
-    parameters
-    ----------
-    pos : np.ndarray
-        The positions of the particles in cartesian space
-    box_dim : float
-        The dimension of the simulation box
-
-    returns
-    -------
-    rel_pos : np.ndarray
-        Relative positions of particles
-    rel_dist : np.ndarray
-        The distance between particles
-    """
-    # create meshgrids to make an n-by-n matrix of distances
-    central_x, other_x = np.meshgrid(pos[:, 0], pos[:, 0])
-    central_y, other_y = np.meshgrid(pos[:, 1], pos[:, 1])
-    central_z, other_z = np.meshgrid(pos[:, 2], pos[:, 2])
-    # moving to the coordinate frame of the central particle
-    # to find the closest position of those around
-    x_dist = (central_x - other_x + box_dim[0] / 2) % box_dim[0] - box_dim[0] / 2
-    y_dist = (central_y - other_y + box_dim[1] / 2) % box_dim[1] - box_dim[1] / 2
-    z_dist = (central_z - other_z + box_dim[2] / 2) % box_dim[2] - box_dim[2] / 2
-
-    relative_positions = np.stack([x_dist, y_dist, z_dist])
-    distances = np.ma.masked_values(
-        np.sqrt(x_dist * x_dist + y_dist * y_dist + z_dist * z_dist),
-        0.0,
-        rtol=1e-60,
-        atol=1e-60,
-    )
-    dprint(f"there are {np.ma.count_masked(distances)} masked distance values")
-
-    # return the full distance matrix of shape n-by-n
-    return (relative_positions, distances)
-
-
-def lj_force(rel_pos, rel_dist):  # units of epsilon/sigma
-    """
-    Calculates the net forces on each atom from the matrices containing the positions and distances.
-
-    Parameters
-    ----------
-    rel_pos : np.ndarray
-        Relative particle positions as obtained from atomic_distances
-    rel_dist : np.ndarray
-        Relative particle distances as obtained from atomic_distances
-
-    Returns
-    -------
-    np.ndarray
-        nx3 array having the net vector force acting on particle i due to all other particles
-    """
-    # Compute force magnitude using the Lennard-Jones force formula
-    force_magnitude = 24 * (1 / rel_dist) ** 7 - 48 * (1 / rel_dist) ** 13
-    dprint(
-        f"the minimal force magnitude is {np.min(force_magnitude)} and the maximum force is {np.max(force_magnitude)}"
-    )
-
-    # Compute force matrix
-    force_direction = rel_pos / rel_dist
-
-    force_matrix = force_magnitude * force_direction
-
-    # Sum forces acting on each particle
-    net_force = -np.sum(force_matrix, axis=1)
-
-    return net_force.T
-
-
-def kinetic_energy(vel):  # units of epsilon
-    """
-    Computes the kinetic energy of an atomic system.
-
-    Parameters
-    ----------
-    vel: np.ndarray
-        Velocity of particle
-
-    Returns
-    -------
-    float
-        The total kinetic energy of the system.
-    """
-    # Kinetic energy for each particle
-    ke_individual = 1 / 2 * np.sum(vel**2, axis=1)
-    # Total Kinetic Energy
-    ke = np.sum(ke_individual)
-    return ke
-
-
-def lj_potential(distance):  # units of epsilon
-    return 4 * ((1 / distance) ** 12 - (1 / distance) ** 6)
-
-
-def potential_energy(rel_dist):
-    """
-    Computes the potential energy of an atomic system.
-
-    Parameters
-    ----------
-    rel_dist : np.ndarray
-        Relative particle distances as obtained from atomic_distances
-
-    Returns
-    -------
-    float
-        The total potential energy of the system.
-    """
-
-    return 1 / 2 * np.sum(lj_potential(rel_dist))
 
 
 def plot_energy(kinetic, potential, file_name="energies.png"):
@@ -301,6 +111,7 @@ if __name__ == "__main__":
         random_seed,
         position_init_method,
         velocity_init_method,
+        simulator_type,
     ) = parse_config("config.json")
     print(
         f"simulating {amount_of_particles} particles for {timesteps} timesteps, with a time step size of {step_size}"
@@ -329,8 +140,20 @@ if __name__ == "__main__":
             f"Please select a valid velocity init method ('zero', 'mbdist'), currently: {velocity_init_method}"
         )
         exit(3)
+        
+    simulator = None
+    if simulator_type == "verlet":
+        simulator = simulators.verlet
+    elif simulator_type == "euler":
+        simulator = simulators.euler
+    else:
+        print(
+            f"Please select a valid simulator ('verlet', 'euler'), currently: {simulator_type}"
+        )
+        exit(4)
+    
 
-    pos, vel, kinetic, potential, distance_list = simulate(
+    pos, vel, kinetic, potential, distance_list = simulator(
         init_pos,
         init_vel,
         timesteps,
